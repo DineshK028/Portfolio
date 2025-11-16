176b1db1eaae891080a4696b53a8a6f160856544
DineshK028 <163395592+DineshK028@users.noreply.github.com>
2025-11-16 13:26:50 +0530

    Revise tech stack and add new icons
    Updated technology stack icons and added new technologies.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from "react";
-import styled from "styled-components";
 import {
-  FaGithub,
-  FaLinkedin,
-  FaEnvelope,
-  FaExternalLinkAlt,
   FaJava,
-  FaCode,
-  FaPython,
-  FaJs,
   FaReact,
-  FaNodeJs,
-  FaHtml5,
-  FaCss3Alt,
-  FaServer,
-  FaBrain,
-  FaBriefcase, // For Full Stack Developer icon
-  FaLightbulb, // For Problem Solver icon
+  FaDocker,
+  FaDatabase,
+  FaBrain
 } from "react-icons/fa";
-import { SiExpress, SiTailwindcss, SiMongodb } from "react-icons/si";
-import Navbar from "./components/Navbar";
+import { SiSpring, SiMongodb, SiMysql, SiEclipseide, SiMaven } from "react-icons/si";
+
+const techStacks = [
+  { name: "Java", icon: <FaJava /> },
+  { name: "JEE", icon: <FaJava /> },
+  { name: "Hibernate", icon: <FaDatabase /> },
+  { name: "Spring", icon: <SiSpring /> },
+  { name: "Spring Boot", icon: <SiSpring /> },
+  { name: "ReactJS", icon: <FaReact /> },
+  { name: "Docker", icon: <FaDocker /> },
+  { name: "Maven", icon: <SiMaven /> },
+  { name: "MySQL", icon: <SiMysql /> },
+  { name: "MongoDB", icon: <SiMongodb /> },
+  { name: "Eclipse", icon: <SiEclipseide /> },
+  { name: "Machine Learning", icon: <FaBrain /> },
+  { name: "CNN", icon: <FaBrain /> },
+];
 
 // Theme colors
 const theme = {
